3b765065fd95f1257841f71576aa5623b35b3323
Maurício Theodoro <138634517+mauricio-theodoro@users.noreply.github.com>
2024-11-23 10:15:25 -0300

    Corrige o componente Movie e tratamento de erros

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,4 +1,89 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+import "./Movie.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
-  return <div>Movie</div>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getMovie = async (url) => {
+    try {
+      const res = await fetch(url);
+
+      if (!res.ok) {
+        throw new Error(`Erro: ${res.status}`);
+      }
+
+      const data = await res.json();
+      setMovie(data || []);
+    } catch (err) {
+      setError("Não foi possível carregar o filme.");
+      console.error("Erro na requisição:", err.message);
+    } finally {
+      setLoading(false); // Atualiza o estado de carregamento
+    }
+  };
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?api_key=${apiKey}`;
+    getMovie(movieUrl);
+  }, [id]);
+
+  if (loading) {
+    return <div className="loading">Carregando...</div>;
+  }
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  return (
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 /> Orçamento:
+            </h3>
+            <p>{movie.budget}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{movie.revenue}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
+
 export default Movie;
